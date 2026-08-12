a1a664850035a4c421f22be42536e1e6babd7051
WYCwyc0508 <19706868112@163.com>
2026-08-12 15:16:11 +0800

    style: remove dark mode entirely

--- docs/assets/javascripts/posting.js
+++ docs/assets/javascripts/posting.js
@@ -1,8 +1,4 @@
 (function () {
-  if (location.pathname === "/" || location.pathname === "/index.html") {
-    document.body.classList.add("is-home");
-  }
-
   var CONFIG = {
     repo: "WYCwyc0508/WYCwyc0508.github.io",
     branch: "main",
